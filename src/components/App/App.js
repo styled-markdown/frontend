988cee3ad707defb8ddc642c6c91452c27d8f557
Seoji Kang <lazymoledg@gmail.com>
2022-03-08 02:56:48 +0900

    [10] 메인페이지 및 로그인 화면 만들기
    - Feat: firebase 기반 로그인 추가
    - Feat: react-query, react-router-dom 세팅

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
+import { queryClient } from "../../reactQuery";
 import AppHeader from "../AppHeader/AppHeader";
-import Editor from "../Editor/Editor";
-import ExportToolbar from "../ExportToolbar/ExportToolbar";
-
-const AppSection = styled.section`
-  padding: 20px;
-
-  .welcome {
-    margin: 10px;
-    text-align: center;
-  }
-`;
-
-const AppArticle = styled.article`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 10px;
-  padding: 10px;
-`;
-
-const ExportToolbarContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50px;
-`;
+import IndexPage from "../IndexPage/IndexPage";
 
 export default function App() {
   return (
     <RecoilRoot>
-      <AppHeader />
-      <AppSection>
-        <h1 className="welcome">어서오세요, Styled-markdown은 처음이신가요?</h1>
-        <AppArticle>
-          <Editor />
-        </AppArticle>
-        <ExportToolbarContainer>
-          <ExportToolbar />
-        </ExportToolbarContainer>
-      </AppSection>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AppHeader />
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+          </Routes>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
---
+++ src/components/IndexPage/IndexPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+import Editor from "../Editor/Editor";
+import ExportToolbar from "../ExportToolbar/ExportToolbar";
+
+const AppSection = styled.section`
+  padding: 20px;
+
+  .welcome {
+    margin: 10px;
+    text-align: center;
+  }
+`;
+
+const AppArticle = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const ExportToolbarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+`;
+
+export default function IndexPage() {
+  return (
+    <AppSection>
+      <h1 className="welcome">어서오세요, Styled-markdown은 처음이신가요?</h1>
+      <AppArticle>
+        <Editor />
+      </AppArticle>
+      <ExportToolbarContainer>
+        <ExportToolbar />
+      </ExportToolbarContainer>
+    </AppSection>
+  );
+}
